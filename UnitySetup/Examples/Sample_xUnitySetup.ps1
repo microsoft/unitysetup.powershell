9037f8e19fedbc261e7713cc2d16de5f8eb3d5a0
Josh Wittner <jowitt@microsoft.com>
2018-03-07 13:53:27 -0800

    Added xUnity DSC Resource

---
+++ UnitySetup/Examples/Sample_xUnitySetup.ps1
@@ -0,0 +1,28 @@
+<#
+    Create a custom configuration by passing in necessary values
+#>
+Configuration Sample_xUnitySetup {
+    param 
+    (       
+        [System.String]
+        $Versions = '2017.3.0f3,2018.1.0b9',
+
+        [ValidateSet('Present', 'Absent')]
+        [System.String]
+        $Ensure = 'Present',
+
+        [System.String[]]
+        $Components = @('Setup','Linux','Metro','Mac','iOS')
+    )
+
+    Import-DscResource -ModuleName UnitySetup
+
+    Node 'localhost' {
+
+        xUnitySetup Unity {
+            Versions = $Versions
+            Components = $Components
+            Ensure = $Ensure
+        }
+    }
+}
