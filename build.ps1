2d28258b5bdc066e90329c38ea9d7b0ed8ec8dec
Josh Wittner <jowitt@microsoft.com>
2018-05-10 14:51:28 -0700

    Added copyrights

--- build.ps1
+++ build.ps1
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
 param([int]$Revision = 0, [string]$Suffix = '')
 Import-Module 'PowerShellGet' -Force
 
