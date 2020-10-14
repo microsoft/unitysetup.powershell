eb60113dc1e94fbf91a1e91b4718c59d3d50f1b7
Mio Bambino <DarkMio@users.noreply.github.com>
2020-10-14 11:50:45 +0200

    Allow current set of test platforms
    As per Unity Documentation: https://docs.unity3d.com/Packages/com.unity.test-framework@1.1/manual/reference-command-line.html
    
    The wording on the documentation is a bit bad, however it allows to pass the test platform directly, switching automatically between EditMode und PlayMode and setting the BuildTarget directly. This is intended behaviour and exists since the integration of the Unity Testrunner, thus is downward compatible.

--- UnitySetup/UnitySetup.psm1
+++ UnitySetup/UnitySetup.psm1
@@ -1715,7 +1715,7 @@ function Start-UnityEditor {
         [parameter(Mandatory = $false)]
         [switch]$RunEditorTests,
         [parameter(Mandatory = $false)]
-        [ValidateSet('EditMode', 'PlayMode')]
+        [ValidateSet('EditMode', 'PlayMode', 'StandaloneWindows', 'StandaloneWindows64', 'StandaloneLinux64', 'StandaloneOSX', 'iOS', 'Android', 'PS4', 'XboxOne')]
         [string]$TestPlatform,
         [parameter(Mandatory = $false)]
         [string]$TestResults,
