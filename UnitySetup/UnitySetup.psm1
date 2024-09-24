18e64ef4c699a5c54d475df42fe0353c239d8415
Michael Smith <mismit@microsoft.com>
2024-09-24 14:21:27 -0700

    Explicitly print formatted table or 'no changes were made'

--- UnitySetup/UnitySetup.psm1
+++ UnitySetup/UnitySetup.psm1
@@ -2823,13 +2823,19 @@ function Update-UnityPackageManagerConfig {
             Export-UPMConfig -UPMConfig $upmConfigs -tomlFilePaths $tomlFilePaths
         }
 
-        Write-Verbose "Summary"
-        $upmConfigs | ForEach-Object {
-            [PSCustomObject]@{
-                ScopedURL = $_.ScopedURL
-                Succeeded = -not [string]::IsNullOrEmpty($_.Auth)
-            }
-        } | Format-Table -AutoSize
+        if ($upmConfigs) {
+            Write-Verbose "Summary"
+            $upmConfigs = $upmConfigs | ForEach-Object {
+                [PSCustomObject]@{
+                    ScopedURL = $_.ScopedURL
+                    Succeeded = -not [string]::IsNullOrEmpty($_.Auth)
+                }
+            } | Format-Table -AutoSize
+        
+            Write-Verbose $upmConfigs
+        } else {
+            Write-Verbose "No changes were made"
+        }
     }
 
     if ($VerifyOnly) {
